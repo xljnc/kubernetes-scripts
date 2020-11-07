0dfc0847927c36a723d9381a07b350a97a9bf536
qiyu <jsj0804wt@126.com>
2020-11-07 21:53:44 +0800

    rocketmq

---
+++ rocketmq/start_mq.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+kubectl create -f  ui-rc.yml
+kubectl create -f  ui-service.yml
+
+
+kubectl create -f  broker-s-rc.yml
+kubectl create -f  broker-s-service.yml
+
+
+
+kubectl create -f  broker-rc.yml
+kubectl create -f  broker-service.yml
+
+
+kubectl create -f  rocketmq-rc.yml
+kubectl create -f  rocketmq-service.yml
+
+
+echo "create  ui  success"
+echo "create  broker-s  success"
+echo "create  broker  success"
+echo "create  rocketmq  success"
